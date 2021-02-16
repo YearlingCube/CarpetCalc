b6afb539b710796a67d8b72ed016a66baff929a7
YearlingCube <73756468+YearlingCube@users.noreply.github.com>
2021-02-16 09:36:45 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,375 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CurrencyCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.WidthTextBox = New System.Windows.Forms.TextBox()
+        Me.CarpetButton = New System.Windows.Forms.RadioButton()
+        Me.TileButton = New System.Windows.Forms.RadioButton()
+        Me.WoodButton = New System.Windows.Forms.RadioButton()
+        Me.QuartzButton = New System.Windows.Forms.RadioButton()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LengthTextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SqGroupBox = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.CostLabel = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TaxLabel = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.GrandLabel = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.CostPerLabel = New System.Windows.Forms.Label()
+        Me.CalcButton = New System.Windows.Forms.Button()
+        Me.SqGroupBox.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Mongolian Baiti", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(259, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(265, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Bob's Floor Emporium"
+        '
+        'WidthTextBox
+        '
+        Me.WidthTextBox.Location = New System.Drawing.Point(38, 47)
+        Me.WidthTextBox.Name = "WidthTextBox"
+        Me.WidthTextBox.Size = New System.Drawing.Size(39, 20)
+        Me.WidthTextBox.TabIndex = 1
+        '
+        'CarpetButton
+        '
+        Me.CarpetButton.AutoSize = True
+        Me.CarpetButton.Location = New System.Drawing.Point(94, 120)
+        Me.CarpetButton.Name = "CarpetButton"
+        Me.CarpetButton.Size = New System.Drawing.Size(56, 17)
+        Me.CarpetButton.TabIndex = 2
+        Me.CarpetButton.TabStop = True
+        Me.CarpetButton.Text = "Carpet"
+        Me.CarpetButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.CarpetButton.UseVisualStyleBackColor = True
+        '
+        'TileButton
+        '
+        Me.TileButton.AutoSize = True
+        Me.TileButton.Location = New System.Drawing.Point(95, 173)
+        Me.TileButton.Name = "TileButton"
+        Me.TileButton.Size = New System.Drawing.Size(42, 17)
+        Me.TileButton.TabIndex = 3
+        Me.TileButton.TabStop = True
+        Me.TileButton.Text = "Tile"
+        Me.TileButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.TileButton.UseVisualStyleBackColor = True
+        '
+        'WoodButton
+        '
+        Me.WoodButton.AutoSize = True
+        Me.WoodButton.Location = New System.Drawing.Point(95, 231)
+        Me.WoodButton.Name = "WoodButton"
+        Me.WoodButton.Size = New System.Drawing.Size(54, 17)
+        Me.WoodButton.TabIndex = 4
+        Me.WoodButton.TabStop = True
+        Me.WoodButton.Text = "Wood"
+        Me.WoodButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.WoodButton.UseVisualStyleBackColor = True
+        '
+        'QuartzButton
+        '
+        Me.QuartzButton.AutoSize = True
+        Me.QuartzButton.Location = New System.Drawing.Point(95, 286)
+        Me.QuartzButton.Name = "QuartzButton"
+        Me.QuartzButton.Size = New System.Drawing.Size(56, 17)
+        Me.QuartzButton.TabIndex = 5
+        Me.QuartzButton.TabStop = True
+        Me.QuartzButton.Text = "Quartz"
+        Me.QuartzButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.QuartzButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(35, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 16)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Square Footage:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LengthTextBox
+        '
+        Me.LengthTextBox.Location = New System.Drawing.Point(105, 47)
+        Me.LengthTextBox.Name = "LengthTextBox"
+        Me.LengthTextBox.Size = New System.Drawing.Size(39, 20)
+        Me.LengthTextBox.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(83, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "By"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(42, 70)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Width"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(104, 69)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Length"
+        '
+        'SqGroupBox
+        '
+        Me.SqGroupBox.AutoSize = True
+        Me.SqGroupBox.Controls.Add(Me.Label2)
+        Me.SqGroupBox.Controls.Add(Me.Label5)
+        Me.SqGroupBox.Controls.Add(Me.WidthTextBox)
+        Me.SqGroupBox.Controls.Add(Me.Label4)
+        Me.SqGroupBox.Controls.Add(Me.LengthTextBox)
+        Me.SqGroupBox.Controls.Add(Me.Label3)
+        Me.SqGroupBox.Location = New System.Drawing.Point(244, 102)
+        Me.SqGroupBox.Name = "SqGroupBox"
+        Me.SqGroupBox.Size = New System.Drawing.Size(200, 100)
+        Me.SqGroupBox.TabIndex = 15
+        Me.SqGroupBox.TabStop = False
+        Me.SqGroupBox.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Yi Baiti", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(560, 173)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 20)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Cost:"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CostLabel
+        '
+        Me.CostLabel.Font = New System.Drawing.Font("Microsoft Yi Baiti", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CostLabel.Location = New System.Drawing.Point(608, 171)
+        Me.CostLabel.Name = "CostLabel"
+        Me.CostLabel.Size = New System.Drawing.Size(154, 33)
+        Me.CostLabel.TabIndex = 17
+        Me.CostLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Yi Baiti", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(560, 199)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(42, 20)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Tax:"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TaxLabel
+        '
+        Me.TaxLabel.Font = New System.Drawing.Font("Microsoft Yi Baiti", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TaxLabel.Location = New System.Drawing.Point(597, 199)
+        Me.TaxLabel.Name = "TaxLabel"
+        Me.TaxLabel.Size = New System.Drawing.Size(157, 30)
+        Me.TaxLabel.TabIndex = 19
+        Me.TaxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Yi Baiti", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(560, 102)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(108, 20)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "Grand Total:"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GrandLabel
+        '
+        Me.GrandLabel.Font = New System.Drawing.Font("Microsoft Yi Baiti", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GrandLabel.Location = New System.Drawing.Point(663, 100)
+        Me.GrandLabel.Name = "GrandLabel"
+        Me.GrandLabel.Size = New System.Drawing.Size(138, 37)
+        Me.GrandLabel.TabIndex = 21
+        Me.GrandLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.CarpetCalc.My.Resources.Resources.Quartz
+        Me.PictureBox4.Location = New System.Drawing.Point(13, 270)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(76, 50)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 9
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.CarpetCalc.My.Resources.Resources.Wood
+        Me.PictureBox3.Location = New System.Drawing.Point(13, 214)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(76, 50)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.CarpetCalc.My.Resources.Resources.Tile
+        Me.PictureBox2.Location = New System.Drawing.Point(13, 158)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(76, 50)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.CarpetCalc.My.Resources.Resources.Carpet
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 102)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(76, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(106, 37)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(87, 16)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "Cost per Sqft:"
+        '
+        'CostPerLabel
+        '
+        Me.CostPerLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CostPerLabel.Location = New System.Drawing.Point(189, 38)
+        Me.CostPerLabel.Name = "CostPerLabel"
+        Me.CostPerLabel.Size = New System.Drawing.Size(94, 15)
+        Me.CostPerLabel.TabIndex = 23
+        '
+        'CalcButton
+        '
+        Me.CalcButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CalcButton.Location = New System.Drawing.Point(289, 217)
+        Me.CalcButton.Name = "CalcButton"
+        Me.CalcButton.Size = New System.Drawing.Size(105, 38)
+        Me.CalcButton.TabIndex = 25
+        Me.CalcButton.Text = "Calculate"
+        Me.CalcButton.UseVisualStyleBackColor = True
+        '
+        'CurrencyCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CalcButton)
+        Me.Controls.Add(Me.CostPerLabel)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.GrandLabel)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TaxLabel)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.CostLabel)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.SqGroupBox)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.QuartzButton)
+        Me.Controls.Add(Me.WoodButton)
+        Me.Controls.Add(Me.TileButton)
+        Me.Controls.Add(Me.CarpetButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "CurrencyCalc"
+        Me.Text = "Currency Calculator"
+        Me.SqGroupBox.ResumeLayout(False)
+        Me.SqGroupBox.PerformLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents WidthTextBox As TextBox
+    Friend WithEvents CarpetButton As RadioButton
+    Friend WithEvents TileButton As RadioButton
+    Friend WithEvents WoodButton As RadioButton
+    Friend WithEvents QuartzButton As RadioButton
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LengthTextBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents SqGroupBox As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents CostLabel As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TaxLabel As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents GrandLabel As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents CostPerLabel As Label
+    Friend WithEvents CalcButton As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class CurrencyCalc
+    Dim Area As Integer
+    Dim Widthh As Integer
+    Dim Lengthh As Integer
+    Dim Cost As Decimal
+    Dim Tax As Decimal
+    Dim Grand As Decimal
+    Private Sub QuartzButton_Click(sender As Object, e As EventArgs) Handles WoodButton.Click, TileButton.Click, QuartzButton.Click, CarpetButton.Click
+        Select Case True
+            Case WoodButton.Checked
+                CostPerLabel.Text = 11.ToString("c2")
+                SqGroupBox.Visible = True
+            Case TileButton.Checked
+                CostPerLabel.Text = 9.ToString("c2")
+                SqGroupBox.Visible = True
+            Case QuartzButton.Checked
+                CostPerLabel.Text = 50.ToString("c2")
+                SqGroupBox.Visible = True
+            Case CarpetButton.Checked
+                CostPerLabel.Text = 8.ToString("c2")
+                SqGroupBox.Visible = True
+        End Select
+
+    End Sub
+    Private Sub CalcButton_Click(sender As Object, e As EventArgs) Handles CalcButton.Click
+        Integer.TryParse(WidthTextBox.Text, Widthh)
+        Integer.TryParse(LengthTextBox.Text, Lengthh)
+        AreaCalc()
+        CostCalc()
+    End Sub
+    Function AreaCalc() As Integer
+        Area = Widthh * Lengthh
+        Return True
+    End Function
+    Sub CostCalc()
+        Cost = (Area * CostPerLabel.Text)
+        Tax = (Cost * 0.07)
+        Grand = (Cost + Tax)
+        GrandLabel.Text = "$" & Grand.ToString("n2")
+        CostLabel.Text = "$" & Cost.ToString("n2")
+        TaxLabel.Text = "$" & Tax.ToString("n2")
+    End Sub
+End Class
+' Carpet is 8 Dollars per Sqft
+' Wood is 11 Dollars per Sqft
+' Tile is 9 Dollars per Sqft
+' Quartz is 50 Dollars per Sqft
